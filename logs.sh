53e7cd4b4ed0ec40675759d9a728cdf227dd6ce6
elephunk84 <iaincstott@gmail.com>
2016-10-16 10:34:22 +0100

    update

--- logs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find /var/log -type f -exec file {} \; | grep 'text' | cut -d' ' -f1 | sed -e's/:$//g' | grep -v '[0-9]$' | xargs tail -f
--- run_temp_logger.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/pi/GitRepo/centralheating/resources/python/
-###python ./read_temp.py &
-python ./monitor.py &
