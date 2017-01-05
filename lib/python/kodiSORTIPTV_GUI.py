230ebe555d98596b770a592d6844ba93c2e07cf9
elephunk84 <iaincstott@gmail.com>
2017-01-05 03:22:16 +0000

    backupRoutine

--- lib/python/kodiSORTIPTV_GUI.py
+++ lib/python/kodiSORTIPTV_GUI.py
@@ -1,7 +1,5 @@
 #!/bin/python3
 import datetime, os, sys, collections, glob, re, subprocess, itertools
-from shutil import move
-from tempfile import NamedTemporaryFile
 
 os.chdir("/home/iainstott/Kodi/IPTVLists")
 channelDIR="Channels"
@@ -13,18 +11,22 @@ m3u2FILE=("Archive/"+str(date)+"_2.m3u")
 M3UFILES=[m3u1FILE, m3u2FILE]
 m3uFILE="IPTV.m3u"
 mym3uFILE="MYIPTV.m3u"
+newm3u="NEW.m3u"
 channellist="Channel_LIST.txt"
 fileHEADER="#EXTM3U"
 lineSTART="#EXTINF:-1,"
 
 mychannelsDICT={}
-channels={}
+mychannelsDICTMASTER={}
+availchannelsDICT={}
 
 def buildDICTIONARIES ():
 	with open(channellist, 'rU') as mychannels:
 		for line in mychannels:
 			name=line.rsplit('|',1)
 			name=(name[1])
+			name=name.rstrip('\n')
+			name=name[1:]
 			channel=line.split("|", 1)[0]
 			group=line.split("|",1)[1]
 			group=group.split("|",1)[0]
@@ -34,9 +36,52 @@ def buildDICTIONARIES ():
 		data = availChannels.read().splitlines(True)
 	with open(m3u2FILE, 'w') as fout:
 		fout.writelines(data[1:])
-	with open(m3u2FILE) as f:
-		for line1,line2 in itertools.izip_longest(*[f]*2):
-			print(line1,line2)
+	with open(m3u2FILE, 'r') as f:
+		with open('Channels.txt', 'w') as channelsfile:
+			for line in f:
+				line=line.split(',',1)[1]
+				nextline=next(f)
+				m3ulink=nextline.rstrip('\n')
+				try:
+					line=line.replace(": ", " | ")
+					line=line.replace("USA: ", "USA  | ")
+					line=line.replace("| ", "  | ")
+					line=line.replace(" l ", " | ")
+					line=line.replace("24/7 ", "24/7  | ")
+					line=line.replace("Adult ", "XXX  | Adult ")
+					channelsfile.write(line)
+					name=line.rstrip('\n')
+					name=name.split('| ',1)[1]
+					availchannelsDICT[name]=m3ulink
+				except IndexError:
+					pass
+				continue
+				channelsfile.close()
+		
+def buildMasterDICT ():
+	for key in mychannelsDICT.keys() & availchannelsDICT.keys():
+		name=key
+		data=mychannelsDICT[key]
+		channel=data[0]
+		group=data[1]
+		link=availchannelsDICT[key]
+		data2=[name, group, link]
+		mychannelsDICTMASTER[channel]=data2
+
+def buildM3UFILE ():
+	with open(newm3u, 'w') as f:
+		f.write(fileHEADER+'\n')
+		channels=collections.OrderedDict(sorted(mychannelsDICTMASTER.items()))
+		for key, value in channels.items():
+			name=(value[0])
+			group=(value[1])
+			link=(value[2])
+			f.write(lineSTART+str(group)+' | '+str(name)+'\n')
+			f.write(str(link)+'\n')
+	f.close()
 			
+
 if __name__ == "__main__":
 	buildDICTIONARIES()
+	buildMasterDICT()
+	buildM3UFILE()
